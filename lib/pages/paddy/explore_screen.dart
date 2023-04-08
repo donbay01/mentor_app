bb148e65494fd7fb609db895a2f8f4e94ca68a77
Donbay <donbay01@gmail.com>
2023-04-08 06:01:50 +0100

    added mock

--- lib/pages/paddy/explore_screen.dart
+++ lib/pages/paddy/explore_screen.dart
@@ -11,7 +11,8 @@ class ExploreScreen extends StatefulWidget {
   State<ExploreScreen> createState() => _ExploreScreenState();
 }
 
-class _ExploreScreenState extends State<ExploreScreen> {
+class _ExploreScreenState extends State<ExploreScreen>
+    with TickerProviderStateMixin{
   final List<Mentor> mentors = [
     Mentor('Kokoma', 'Product manager', 'assets/adaptLogo.png', 5, ''),
     Mentor('Chubby', 'E-Learning expert', 'assets/adaptLogo.png', 5, ''),
@@ -23,15 +24,28 @@ class _ExploreScreenState extends State<ExploreScreen> {
   final TextEditingController _searchController = TextEditingController();
 
   List<Mentor> filteredMentors = [];
+  List<Mentor> jobFilter = [];
 
   @override
   void initState() {
+    _tabController = TabController(length: 2, vsync: this);
     super.initState();
     filteredMentors = mentors;
+  }
+  late TabController _tabController;
+
+
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
@@ -56,81 +70,119 @@ class _ExploreScreenState extends State<ExploreScreen> {
                   SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _searchController,
-                          decoration: InputDecoration(
-                            labelText: 'Name, Job, Industry',
-                            hintText: 'mentor@gmail.com',
-                            filled: true,
-                            fillColor: searchColor,
-                            prefixIcon: Icon(Icons.search),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32),
-                              borderSide: const BorderSide(
-                                color: darkBlue,
-                                width: 1.0,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32),
-                              borderSide: const BorderSide(
-                                color: primaryBlue,
-                                width: 1.0,
-                              ),
-                            ),
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              filteredMentors = mentors
-                                  .where((item) => item.description
-                                      .toLowerCase()
-                                      .contains(value.toLowerCase()))
-                                  .toList();
-                            });
-                          },
+                  TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      labelText: 'Name, Job, Industry',
+                      hintText: 'mentor@gmail.com',
+                      filled: true,
+                      fillColor: searchColor,
+                      prefixIcon: Icon(Icons.search),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                        borderSide: const BorderSide(
+                          color: darkBlue,
+                          width: 1.0,
                         ),
                       ),
-                      IconButton(
-                          onPressed: () {}, icon: Icon(Icons.filter_list_sharp))
-                    ],
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                        borderSide: const BorderSide(
+                          color: primaryBlue,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        filteredMentors = mentors
+                            .where((item) => item.description
+                            .toLowerCase()
+                            .contains(value.toLowerCase()))
+                            .toList();
+                        jobFilter = mentors
+                            .where((item) => item.name
+                            .toLowerCase()
+                            .contains(value.toLowerCase()))
+                            .toList();
+                      });
+                    },
                   ),
                   SizedBox(
-                    height: 30,
+                    height: 20,
                   ),
+                  TabBar(
+                      indicatorColor: primaryBlue,
+                      controller: _tabController,
+                      unselectedLabelColor: textGrey,
+                      labelColor: primaryBlue,
+                      indicatorWeight: 2,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      labelStyle: smallBold(primaryBlue),
+                      indicator: const UnderlineTabIndicator(
+                          borderSide: BorderSide(width: 2, color: primaryBlue,)
+                      ),
+                      tabs: [
+                        Tab(
+                          text: 'Career mentorship',
+                        ),
+                        Tab(
+                          text: 'Mock Interview',
+                        )
+                      ]),
+                  SizedBox(height: 20,),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: filteredMentors.length,
-                      itemBuilder: (context, index) {
-                        final mentor = filteredMentors[index];
-                        return Column(
-                          children: [
-                            ListTile(
-                              leading: Image(
-                                image: AssetImage(mentor.image),
-                              ),
-                              title: Text(
-                                mentor.name,
-                                style: mediumBold(darkBlue),
-                              ),
-                              subtitle: Text(mentor.description),
-                            ),
-                            Divider(
-                              height: 2,
-                            )
-                          ],
-                        );
-                      },
-                    ),
-                  ),
+                    child: TabBarView(
+                      controller: _tabController,
+                        children: [
+                      bookingsPage(filteredMentors: filteredMentors,height: height,width: width,),
+                          bookingsPage(filteredMentors: filteredMentors,height: height,width: width,)
+                    ]),
+                  )
                 ],
               ),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class bookingsPage extends StatelessWidget {
+  final double height, width;
+  const bookingsPage({
+    super.key,
+    required this.filteredMentors, required this.height, required this.width,
+  });
+
+  final List<Mentor> filteredMentors;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: filteredMentors.length,
+        itemBuilder: (context, index) {
+          final mentor = filteredMentors[index];
+          return Column(
+            children: [
+              ListTile(
+                leading: Image(
+                  image: AssetImage(mentor.image),
+                ),
+                title: Text(
+                  mentor.name,
+                  style: mediumBold(darkBlue),
+                ),
+                subtitle: Text(mentor.description),
+              ),
+              Divider(
+                height: 2,
+              )
+            ],
+          );
+        },
     );
   }
 }
---
+++ lib/pages/paddy/paddy_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaddyList extends StatelessWidget {
+  const PaddyList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
