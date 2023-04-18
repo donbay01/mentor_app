9dbf7714894d43502daf924d6721d0e9063bd425
Samson Udo <samsonudo@yahoo.com>
2023-04-18 16:03:09 +0100

    Disabled Agora

--- lib/pages/video call/index.dart
+++ lib/pages/video call/index.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+// import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:career_paddy/constants/role.dart';
 import 'package:career_paddy/models/user_model.dart';
 import 'package:career_paddy/services/session.dart';
@@ -25,7 +25,7 @@ class _VideoScreenState extends State<VideoScreen> {
   int? _remoteUid;
   bool _localUserJoined = false;
 
-  late RtcEngine _engine;
+  // late RtcEngine _engine;
   late int uid;
   String? token;
 
@@ -37,61 +37,61 @@ class _VideoScreenState extends State<VideoScreen> {
   }
 
   endCall() async {
-    await _engine.disableVideo();
-    await _engine.stopPreview();
-    await _engine.leaveChannel();
+    // await _engine.disableVideo();
+    // await _engine.stopPreview();
+    // await _engine.leaveChannel();
   }
 
   Future<void> initAgora() async {
     await [Permission.microphone, Permission.camera].request();
     token = await SessionService.getToken(uid, widget.channel);
 
-    _engine = createAgoraRtcEngine();
-    await _engine.initialize(
-      const RtcEngineContext(
-        appId: appId,
-        channelProfile: ChannelProfileType.channelProfileCommunication,
-      ),
-    );
+    // _engine = createAgoraRtcEngine();
+    // await _engine.initialize(
+    //   const RtcEngineContext(
+    //     appId: appId,
+    //     channelProfile: ChannelProfileType.channelProfileCommunication,
+    //   ),
+    // );
 
-    _engine.registerEventHandler(
-      RtcEngineEventHandler(
-        onJoinChannelSuccess: (connection, elapsed) {
-          debugPrint("local user ${connection.localUid} joined");
-          setState(() {
-            _localUserJoined = true;
-          });
-        },
-        onUserJoined: (connection, remoteUid, elapsed) {
-          debugPrint("remote user $remoteUid joined");
-          setState(() {
-            _remoteUid = remoteUid;
-          });
-        },
-        onUserOffline: (connection, remoteUid, reason) {
-          debugPrint("remote user $remoteUid left channel");
-          setState(() {
-            _remoteUid = null;
-          });
-        },
-        onTokenPrivilegeWillExpire: (connection, token) {
-          debugPrint(
-            '[onTokenPrivilegeWillExpire] connection: ${connection.toJson()}, token: $token',
-          );
-        },
-      ),
-    );
+    // _engine.registerEventHandler(
+    //   RtcEngineEventHandler(
+    //     onJoinChannelSuccess: (connection, elapsed) {
+    //       debugPrint("local user ${connection.localUid} joined");
+    //       setState(() {
+    //         _localUserJoined = true;
+    //       });
+    //     },
+    //     onUserJoined: (connection, remoteUid, elapsed) {
+    //       debugPrint("remote user $remoteUid joined");
+    //       setState(() {
+    //         _remoteUid = remoteUid;
+    //       });
+    //     },
+    //     onUserOffline: (connection, remoteUid, reason) {
+    //       debugPrint("remote user $remoteUid left channel");
+    //       setState(() {
+    //         _remoteUid = null;
+    //       });
+    //     },
+    //     onTokenPrivilegeWillExpire: (connection, token) {
+    //       debugPrint(
+    //         '[onTokenPrivilegeWillExpire] connection: ${connection.toJson()}, token: $token',
+    //       );
+    //     },
+    //   ),
+    // );
 
-    await _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
-    await _engine.enableVideo();
-    await _engine.startPreview();
+    // await _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+    // await _engine.enableVideo();
+    // await _engine.startPreview();
 
-    await _engine.joinChannel(
-      token: token!,
-      channelId: widget.channel,
-      uid: uid,
-      options: const ChannelMediaOptions(),
-    );
+    // await _engine.joinChannel(
+    //   token: token!,
+    //   channelId: widget.channel,
+    //   uid: uid,
+    //   options: const ChannelMediaOptions(),
+    // );
   }
 
   @override
@@ -102,45 +102,45 @@ class _VideoScreenState extends State<VideoScreen> {
       ),
       body: Stack(
         children: [
-          Center(
-            child: _remoteVideo(),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: SizedBox(
-              width: 100,
-              height: 150,
-              child: Center(
-                child: _localUserJoined
-                    ? AgoraVideoView(
-                        controller: VideoViewController(
-                          rtcEngine: _engine,
-                          canvas: const VideoCanvas(uid: 0),
-                        ),
-                      )
-                    : const CircularProgressIndicator(),
-              ),
-            ),
-          ),
+          // Center(
+          //   child: _remoteVideo(),
+          // ),
+          // Align(
+          //   alignment: Alignment.topLeft,
+          //   child: SizedBox(
+          //     width: 100,
+          //     height: 150,
+          //     child: Center(
+          //       child: _localUserJoined
+          //           ? AgoraVideoView(
+          //               controller: VideoViewController(
+          //                 rtcEngine: _engine,
+          //                 canvas: const VideoCanvas(uid: 0),
+          //               ),
+          //             )
+          //           : const CircularProgressIndicator(),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
   }
 
-  Widget _remoteVideo() {
-    if (_remoteUid != null) {
-      return AgoraVideoView(
-        controller: VideoViewController.remote(
-          rtcEngine: _engine,
-          canvas: VideoCanvas(uid: _remoteUid),
-          connection: RtcConnection(channelId: widget.channel),
-        ),
-      );
-    } else {
-      return const Text(
-        'Please wait for remote user to join',
-        textAlign: TextAlign.center,
-      );
-    }
-  }
+  // Widget _remoteVideo() {
+  //   if (_remoteUid != null) {
+  //     return AgoraVideoView(
+  //       controller: VideoViewController.remote(
+  //         rtcEngine: _engine,
+  //         canvas: VideoCanvas(uid: _remoteUid),
+  //         connection: RtcConnection(channelId: widget.channel),
+  //       ),
+  //     );
+  //   } else {
+  //     return const Text(
+  //       'Please wait for remote user to join',
+  //       textAlign: TextAlign.center,
+  //     );
+  //   }
+  // }
 }
